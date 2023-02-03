242e17f5c12c6a1cb9356651ef7041ae18cb78db
Ilia Pachulia <ipachulia@evolution.com>
2023-02-03 23:15:54 +0400

    started working on form first page

--- resume-app/src/components/main-components/GeneralInfo.js
+++ resume-app/src/components/main-components/GeneralInfo.js
@@ -1,5 +1,31 @@
+import classes from "./GeneralInfo.module.css";
+
 const GeneralInfo = () => {
-  return <h1>General Info</h1>;
+  return (
+    <main>
+      <div className={classes.container}>
+        <div className={classes.header}>
+          <p className={classes.page_name}>პირადი ინფო</p>
+          <p className={classes.pagination}>1/3</p>
+        </div>
+        <form>
+          <div className={classes.name_container}>
+            <label>
+              სახელი
+              <input type="text" name="name" />
+              <small>მინიმუმ 2 ასო, ქართული ასოები</small>
+            </label>
+            <label>
+              გვარი
+              <input type="text" />
+              <small>მინიმუმ 2 ასო, ქართული ასოები</small>
+            </label>
+          </div>
+        </form>
+      </div>
+      <div className={classes.resume_container}></div>
+    </main>
+  );
 };
 
 export default GeneralInfo;
--- resume-app/src/components/main-components/Landing.js
+++ resume-app/src/components/main-components/Landing.js
@@ -1,10 +1,21 @@
+import classes from "./Landing.module.css";
+import redberryImg from "../../assets/redberry.png";
+import redberryLogo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <div>
-      <img src="../../assets/LOGO-02 3.jpg" alt="redberry-logo" />
-      
-    </div>
+    <main className={classes.container}>
+      <img src={redberryImg} alt="redberry-logo" />
+      <div className={classes.sub_container}>
+        <div className={classes.center_button}>
+          <Link to="/general-info">
+            <button className={classes.button}>რეზიუმეს დამატება</button>
+          </Link>
+          <img src={redberryLogo} alt="redberry-logo" />
+        </div>
+      </div>
+    </main>
   );
 };
 
